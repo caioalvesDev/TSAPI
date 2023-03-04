b8058cdd4a374ac53c1b497f4627da93a3d95338
Caio Alves <kaio_rx@hotmail.com>
2023-03-04 17:43:37 -0300

    Feat: add express

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
+import express, {Express, Request, Response} from 'express';
 
+const server:Express = express();
+const PORT = 3333;
 
-const a: string = 'Typescript'
+server.get('/', (req: Request, res:Response) => {
+    res.send('Hello World');
+})
 
+server.listen(PORT, () => {
+    console.log(`Servidor rodando na porta: ${PORT}`);
 
-console.log(a);
+})
+
 
